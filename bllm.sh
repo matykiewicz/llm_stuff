08284a71ce4e4f7910b8a20ea1fa1f7334f2ee06
Pawel M <pawel.matykiewicz@gmail.com>
2026-03-06 15:59:56 +0000

    More models and benchmarks

--- bllm.sh
+++ bllm.sh
@@ -12,6 +12,21 @@
 #llama-bench -ngl 999 -fa 1 -mmp 0 --model "${MO}"
 #exit
 
+
+for file in $(find models/ -name "Qwen3*.gguf" |  sort | grep -v 000); do
+	echo "$file"
+	distrobox enter llama-rocm-7.2 -- bash -c "llama-bench -ngl 999 -fa 1 -mmp 1 --model /home/pawelm/$file"
+	distrobox enter llama-vulkan-radv -- bash -c "llama-bench -ngl 999 -fa 1 -mmp 1 --model /home/pawelm/$file"
+	distrobox enter llama-vulkan-amdvlk -- bash -c "llama-bench -ngl 999 -fa 1 -mmp 1 --model /home/pawelm/$file"
+	distrobox enter llama-rocm7-nightlies -- bash -c "llama-bench -ngl 999 -fa 1 -mmp 1 --model /home/pawelm/$file"
+	distrobox enter llama-rocm-7.2 -- bash -c "llama-bench -ngl 999 -fa 1 -mmp 0 --model /home/pawelm/$file"
+	distrobox enter llama-vulkan-radv -- bash -c "llama-bench -ngl 999 -fa 1 -mmp 0 --model /home/pawelm/$file"
+	distrobox enter llama-vulkan-amdvlk -- bash -c "llama-bench -ngl 999 -fa 1 -mmp 0 --model /home/pawelm/$file"
+	distrobox enter llama-rocm7-nightlies -- bash -c "llama-bench -ngl 999 -fa 1 -mmp 0 --model /home/pawelm/$file"
+done
+
+exit
+
 distrobox enter llama-rocm-7.2 -- bash -c 'llama-bench -ngl 999 -fa 1 -mmp 1 --model /home/pawelm/models/Qwen3-Coder-30B-A3B-Instruct-UD-Q4_K_XL.gguf'
 distrobox enter llama-vulkan-radv -- bash -c 'llama-bench -ngl 999 -fa 1 -mmp 1 --model /home/pawelm/models/Qwen3-Coder-30B-A3B-Instruct-UD-Q4_K_XL.gguf'
 distrobox enter llama-vulkan-amdvlk -- bash -c 'llama-bench -ngl 999 -fa 1 -mmp 1 --model /home/pawelm/models/Qwen3-Coder-30B-A3B-Instruct-UD-Q4_K_XL.gguf'
--- dllm.sh
+++ dllm.sh
@@ -2,9 +2,70 @@
 
 source ~/.venv/bin/activate
 
-
 EXTR="--dry-run"
 EXTR="--no-dry-run"
+
+REPO="ggml-org/gemma-3-27b-it-qat-GGUF"
+FILE1="gemma-3-27b-it-qat-Q4_0.gguf"
+hf download "${REPO}" "${FILE1}" --local-dir ~/models "${EXTR}"
+
+REPO="ggml-org/InternVL3-14B-Instruct-GGUF"
+FILE1="InternVL3-14B-Instruct-Q8_0.gguf"
+hf download "${REPO}" "${FILE1}" --local-dir ~/models "${EXTR}"
+
+REPO="mradermacher/MiniMax-M2.5-REAP-172B-A10B-i1-GGUF"
+FILE1="MiniMax-M2.5-REAP-172B-A10B.i1-Q4_K_S.gguf"
+FILE2="MiniMax-M2.5-REAP-172B-A10B.i1-Q4_K_M.gguf"
+hf download "${REPO}" "${FILE1}" --local-dir ~/models "${EXTR}"
+hf download "${REPO}" "${FILE2}" --local-dir ~/models "${EXTR}"
+
+REPO="unsloth/gpt-oss-120b-GGUF"
+FILE1="UD-Q4_K_XL/gpt-oss-120b-UD-Q4_K_XL-00001-of-00002.gguf"
+FILE2="UD-Q4_K_XL/gpt-oss-120b-UD-Q4_K_XL-00002-of-00002.gguf"
+hf download "${REPO}" "${FILE1}" --local-dir ~/models "${EXTR}"
+hf download "${REPO}" "${FILE2}" --local-dir ~/models "${EXTR}"
+
+REPO="ggml-org/gpt-oss-120b-GGUF"
+FILE1="gpt-oss-120b-mxfp4-00001-of-00003.gguf"
+FILE2="gpt-oss-120b-mxfp4-00002-of-00003.gguf"
+FILE3="gpt-oss-120b-mxfp4-00003-of-00003.gguf"
+hf download "${REPO}" "${FILE1}" --local-dir ~/models "${EXTR}"
+hf download "${REPO}" "${FILE2}" --local-dir ~/models "${EXTR}"
+hf download "${REPO}" "${FILE3}" --local-dir ~/models "${EXTR}"
+
+REPO="unsloth/Qwen3-Coder-Next-GGUF"
+#FILE1="Qwen3-Coder-Next-UD-Q4_K_XL.gguf"
+FILE2="Qwen3-Coder-Next-IQ4_NL.gguf"
+FILE3="Qwen3-Coder-Next-MXFP4_MOE.gguf"
+FILE4="Qwen3-Coder-Next-Q4_K_M.gguf"
+#hf download "${REPO}" "${FILE1}" --local-dir ~/models "${EXTR}"
+hf download "${REPO}" "${FILE2}" --local-dir ~/models "${EXTR}"
+hf download "${REPO}" "${FILE3}" --local-dir ~/models "${EXTR}"
+hf download "${REPO}" "${FILE4}" --local-dir ~/models "${EXTR}"
+
+FILE1="UD-Q6_K_XL/Qwen3-Coder-Next-UD-Q6_K_XL-00001-of-00003.gguf"
+FILE2="UD-Q6_K_XL/Qwen3-Coder-Next-UD-Q6_K_XL-00002-of-00003.gguf"
+FILE3="UD-Q6_K_XL/Qwen3-Coder-Next-UD-Q6_K_XL-00003-of-00003.gguf"
+hf download "${REPO}" "${FILE1}" --local-dir ~/models "${EXTR}"
+hf download "${REPO}" "${FILE2}" --local-dir ~/models "${EXTR}"
+hf download "${REPO}" "${FILE3}" --local-dir ~/models "${EXTR}"
+
+FILE1="UD-Q5_K_XL/Qwen3-Coder-Next-UD-Q5_K_XL-00001-of-00003.gguf"
+FILE2="UD-Q5_K_XL/Qwen3-Coder-Next-UD-Q5_K_XL-00002-of-00003.gguf"
+FILE3="UD-Q5_K_XL/Qwen3-Coder-Next-UD-Q5_K_XL-00003-of-00003.gguf"
+hf download "${REPO}" "${FILE1}" --local-dir ~/models "${EXTR}"
+hf download "${REPO}" "${FILE2}" --local-dir ~/models "${EXTR}"
+hf download "${REPO}" "${FILE3}" --local-dir ~/models "${EXTR}"
+
+exit
+
+REPO="unsloth/Qwen3-Coder-30B-A3B-Instruct-GGUF"
+FILE1="Qwen3-Coder-30B-A3B-Instruct-Q4_K_M.gguf"
+hf download "${REPO}" "${FILE1}" --local-dir ~/models "${EXTR}"
+
+REPO="unsloth/Qwen3-30B-A3B-Instruct-2507-GGUF"
+FILE1="Qwen3-30B-A3B-Instruct-2507-UD-Q6_K_XL.gguf"
+hf download "${REPO}" "${FILE1}" --local-dir ~/models "${EXTR}"
 
 
 REPO="unsloth/Qwen3-VL-30B-A3B-Instruct-GGUF"
@@ -32,31 +93,6 @@ FILE1="Qwen3-VL-30B-A3B-Instruct-abliterated.Q5_K_M.gguf"
 FILE2="Qwen3-VL-30B-A3B-Instruct-abliterated.Q4_K_M.gguf"
 hf download "${REPO}" "${FILE1}" --local-dir ~/models "${EXTR}"
 hf download "${REPO}" "${FILE2}" --local-dir ~/models "${EXTR}"
-
-
-REPO="unsloth/Qwen3-Coder-Next-GGUF"
-FILE1="Qwen3-Coder-Next-UD-Q4_K_XL.gguf"
-FILE2="Qwen3-Coder-Next-IQ4_NL.gguf"
-FILE3="Qwen3-Coder-Next-MXFP4_MOE.gguf"
-FILE4="Qwen3-Coder-Next-Q4_K_M.gguf"
-hf download "${REPO}" "${FILE1}" --local-dir ~/models "${EXTR}"
-hf download "${REPO}" "${FILE2}" --local-dir ~/models "${EXTR}"
-hf download "${REPO}" "${FILE3}" --local-dir ~/models "${EXTR}"
-hf download "${REPO}" "${FILE4}" --local-dir ~/models "${EXTR}"
-
-FILE1="UD-Q6_K_XL/Qwen3-Coder-Next-UD-Q6_K_XL-00001-of-00003.gguf"
-FILE2="UD-Q6_K_XL/Qwen3-Coder-Next-UD-Q6_K_XL-00002-of-00003.gguf"
-FILE3="UD-Q6_K_XL/Qwen3-Coder-Next-UD-Q6_K_XL-00003-of-00003.gguf"
-hf download "${REPO}" "${FILE1}" --local-dir ~/models "${EXTR}"
-hf download "${REPO}" "${FILE2}" --local-dir ~/models "${EXTR}"
-hf download "${REPO}" "${FILE3}" --local-dir ~/models "${EXTR}"
-
-FILE1="UD-Q5_K_XL/Qwen3-Coder-Next-UD-Q5_K_XL-00001-of-00003.gguf"
-FILE2="UD-Q5_K_XL/Qwen3-Coder-Next-UD-Q5_K_XL-00002-of-00003.gguf"
-FILE3="UD-Q5_K_XL/Qwen3-Coder-Next-UD-Q5_K_XL-00003-of-00003.gguf"
-hf download "${REPO}" "${FILE1}" --local-dir ~/models "${EXTR}"
-hf download "${REPO}" "${FILE2}" --local-dir ~/models "${EXTR}"
-hf download "${REPO}" "${FILE3}" --local-dir ~/models "${EXTR}"
 
 exit
 
